8e3f402a18a8f37a0eaac727087987d54a331d38
Abhinav <abnvv07@gmail.com>
2026-03-27 12:01:19 +0530

    login and register api

--- server/config/settings.py
+++ server/config/settings.py
@@ -151,5 +151,5 @@ from datetime import timedelta
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'AUTH_HEADER_TYPE' : ('Bearer',),
+    'AUTH_HEADER_TYPES' : ('Bearer',),
 }
