3fd76a412712c3c6bd47a6d714251d214332adee
Abhinav <abnvv07@gmail.com>
2026-04-11 15:25:59 +0530

    changed to microservices,two services gateway nginx

--- server/apps/workspaces/routing.py
+++ server/apps/workspaces/routing.py
@@ -1,15 +1,14 @@
 from django.urls import re_path
-
 from apps.notifications import consumers as notif_consumers
-
-# Use absolute paths to avoid naming conflicts
 from apps.workspaces import consumers as chat_consumers
 
 websocket_urlpatterns = [
-    # Explicitly using the renamed imports
     re_path(
-        r"^ws/chat/(?P<workspace_id>\d+)/$",
+        r"ws/chat/(?P<workspace_id>\d+)/$", 
         chat_consumers.WorkspaceChatConsumer.as_asgi(),
     ),
-    re_path(r"^ws/notifications/$", notif_consumers.NotificationConsumer.as_asgi()),
+    re_path(
+        r"ws/notifications/$", 
+        notif_consumers.NotificationConsumer.as_asgi()
+    ),
 ]
